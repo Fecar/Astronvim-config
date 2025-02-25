5bf03bf6070bd1974ab3d0ce6d42e62c21373c81
fecar <fecar95@gmail.com>
2025-02-25 18:15:19 -0300

    First Config

---
+++ community.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
+  -- import/override with your plugins folder
+}
