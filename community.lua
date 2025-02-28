d03832d8eb47af2080adccf49cde4650c7e1f533
fecar <fecar95@gmail.com>
2025-02-28 08:30:01 -0300

    New configs

--- community.lua
+++ community.lua
@@ -2,6 +2,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
   -- import/override with your plugins folder
